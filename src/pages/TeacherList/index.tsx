f7b1ec41d2f2ad6f36559d5f6fa24a54b1b64ab0
Filipe Moreira <filimor@posteo.com.br>
2020-08-04 00:19:13 -0300

    Add TeacherList main section mobile first

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
@@ -22,6 +23,32 @@ function TeacherList() {
                     </div>
                 </form>
             </PageHeader>
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://userstock.io/data/wp-content/uploads/2017/09/michael-dam-258165-scaled.jpg" alt="Gabriela Soares"/>
+                        <div>
+                            <strong>Gabriela Soares</strong>
+                            <span>Matemática</span>
+                        </div>
+                    </header>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, impedit! Aperiam fugiat quos adipisci sint saepe nesciunt. Dicta ducimus corporis cum reiciendis voluptates ad obcaecati repellat laudantium facere rem. Quaerat.
+                        <br/><br/>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, quasi dolorum suscipit nesciunt eius porro rerum optio eos dicta aut asperiores modi error rem molestias harum reprehenderit praesentium neque doloribus.
+                    </p>
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="Whatsapp"/>
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     );
 }

13ae4b990cbb8e8ba74d622530bd23dcdc49369f
Filipe Moreira <filimor@posteo.com.br>
2020-08-06 14:21:11 -0300

    Start implementing teacher form

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import './styles.css';
 
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Estes são os proffys disponíveis." />
+            <PageHeader title="Estes são os proffys disponíveis."
+            description="O primeiro passo é preencher esse formulário de inscrição." />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+                    <div className="input-block">
+                        <label htmlFor="name">Nome completo</label>
+                        <input type="text" id="name"/>
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="Avatar">Avatar</label>
+                        <input type="text" id="Avatar"/>
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="whatsapp">Whatsapp</label>
+                        <input type="text" id="whatsapp"/>
+                    </div>
+                </fieldset>
+            </main>
         </div>
     );
 }
